1ff1faec8d22b5ccf812dcfca80250b257cf3927
asror-coder1 <afayzullayev499@gmail.com>
2026-01-07 21:08:27 +0500

    add project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,24 +8,126 @@ public class Main {
 
 //        Darsni bodhladik
 
-        List<String> ismlar = new ArrayList<>();
+//        List<String> ismlar = new ArrayList<>();
+//
+//        ismlar.add("Ali");
+//        ismlar.add("Vali");
+//        ismlar.add("Eshmat");
+//        ismlar.add("Toshmat");
+//        ismlar.add("Erkin");
+//        ismlar.add("Umar");
+//        ismlar.add("Ansor");
+//        ismlar.add("Asror");
+//
+//
+//        int olchami = ismlar.size();
+//        ismlar.remove(3);
+//        System.out.println("Ismloarning o'lchami :" + olchami);
+//
+//        for (String name : ismlar){
+//            System.out.println(name);
+//        }
 
-        ismlar.add("Ali");
-        ismlar.add("Vali");
-        ismlar.add("Eshmat");
-        ismlar.add("Toshmat");
-        ismlar.add("Erkin");
-        ismlar.add("Umar");
-        ismlar.add("Ansor");
-        ismlar.add("Asror");
+
+        Collection<String> col = new ArrayList<>();
+        col.add("Ali");
+        col.add("Vali");
+
+        System.out.println(col.size());
+
+//        ============================
+
+//        ArrayList
+//       ✔ Tez o‘qiladi
+//       ❌ O‘rtadan o‘chirish sekin
+
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("A");
+
+        System.out.println(list.get(1));
 
 
-        int olchami = ismlar.size();
+//        LinkedList
+//        ✔ Qo‘shish/o‘chirish tez
+//        ❌ O‘qish sekinroq
 
-        System.out.println("Ismloarning o'lchami :" + olchami);
+        List<Integer> nums = new LinkedList<>();
+        nums.add(10);
+        nums.add(20);
 
-        for (String name : ismlar){
-            System.out.println(name);
+
+//        HashSet
+//       ❌ Tartib yo‘q
+//       ✔ Juda tez
+
+        Set<String> set = new HashSet<>();
+        set.add("A");
+        set.add("B");
+        set.add("A");
+
+        System.out.println(set);
+
+//        LinkedHashSet
+//       ✔ Qo‘shilgan tartib saqlanadi
+//       TreeSet
+//       ✔ Avtomatik tartiblaydi
+
+        Set<Integer> sett = new TreeSet<>();
+        sett.add(5);
+        sett.add(1);
+        sett.add(3);
+
+        System.out.println(sett);
+
+//        7️⃣ Queue — navbat
+//        👉 FIFO (birinchi kirgan – birinchi chiqadi)
+//         Turlari:
+//         PriorityQueue
+//         ArrayDeque
+
+
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("A");
+        queue.add("B");
+
+        System.out.println(queue.poll());
+
+//        8️⃣ Map — kalit → qiymat (Collection emas)
+//        👉 Key unique
+//        👉 Value takror bo‘lishi mumkin
+//         Turlari:
+//         HashMap
+//         LinkedHashMap
+//         TreeMap
+//         HashMap
+
+        Map<Integer , String > map = new HashMap<>();
+        map.put(1 , "Ali");
+        map.put(2 , "Vali");
+
+        System.out.println(map.get(1));
+
+
+//        TreeMap (tartibli)
+
+
+        Map<Integer , String> maps = new TreeMap<>();
+
+//        9️⃣ Iterator — aylanish (loop)
+//
+//        Collection’ni aylanib chiqish uchun.
+
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
+//        Yoki oddiy:
+
+        for (String s : list){
+            System.out.println(s);
         }
     }
 }
